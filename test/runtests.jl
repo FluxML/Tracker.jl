ceba6f48fe79250feb0ecdeb999f4359186eaa3e
Mike J Innes <mike.j.innes@gmail.com>
2019-03-08 13:09:21 +0000

    move jacobian test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,15 @@ Random.seed!(0)
 
 include("tracker.jl")
 
+using Tracker: jacobian
+
+@testset "Jacobian" begin
+  A = param(randn(2,2))
+  x = randn(2)
+  m(x) = A*x
+  y = m(x)
+  J = jacobian(m,x)
+  @test J ≈ A.data
+end
+
 end
