36c5256bcb80a92853cfe6231287e9d9c8dca88e
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-27 15:14:02 -0700

    Make jacobian work with CuArrays

--- src/back.jl
+++ src/back.jl
@@ -173,7 +173,11 @@ Calculate the output jacobian `J = d/dx m(x)` such that each row `i` of `J` corr
 """
 function jacobian(f, x::AbstractVector)
   y::AbstractVector, back = forward(f, x)
-  ȳ(i) = [i == j for j = 1:length(y)]
+  function ȳ(i)
+    δ = fill!(similar(y, Bool), false)
+    δ[i] = true
+    return δ
+  end
   vcat([transpose(back(ȳ(i))[1]) for i = 1:length(y)]...)
 end
 
