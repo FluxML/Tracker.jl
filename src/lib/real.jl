aa8b7d50415f016a2d165b8bf172966271ab5872
Eric Davies <iamed2@gmail.com>
2019-10-23 17:20:21 -0500

    Use stdlib Printf

--- src/lib/real.jl
+++ src/lib/real.jl
@@ -54,7 +54,9 @@ for f in :[isinf, isnan, isfinite].args
   @eval Base.$f(x::TrackedReal) = Base.$f(data(x))
 end
 
-Base.Printf.fix_dec(x::TrackedReal, n::Int, a...) = Base.Printf.fix_dec(data(x), n, a...)
+using Printf
+
+Printf.fix_dec(x::TrackedReal, n::Int, a...) = Printf.fix_dec(data(x), n, a...)
 
 Base.float(x::TrackedReal) = x
 
