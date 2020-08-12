ad66141c7ab2f48fbe64ca5c1d3dee47319f3fa1
Dhairya Gandhi <dhairya@juliacomputing.com>
2020-08-12 08:12:14 +0530

    Revert "Fix StackOverflow errors for long recurrences"

--- test/tracker.jl
+++ test/tracker.jl
@@ -432,14 +432,4 @@ end
   @test back([1, 1]) == (32,)
 end
 
-@testset "Long Recurrences" begin
-  @test Tracker.gradient(rand(10000)) do x
-    s = 0.0
-    for i in 1:length(x)
-      s += x[i]
-    end
-    return s
-  end[1] == ones(10000)
-end
-
 end #testset
