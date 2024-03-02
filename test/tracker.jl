c6ed3b1931989c7698531720fcd5fdc5e0573905
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2024-03-02 09:29:48 -0500

    change test to MersenneTwister to fix 1.6

--- test/tracker.jl
+++ test/tracker.jl
@@ -40,7 +40,7 @@ end
 @test gradtest(x -> logsoftmax(x; dims = 1).*(1:3), (3,5))
 @test gradtest(x -> logsoftmax(x; dims = 2).*(1:3), (3,5))
 
-RNG = NNlib.Random.Xoshiro(1)
+RNG = NNlib.Random.MersenneTwister(1)
 @test gradtest(x -> dropout(copy(RNG), x, 0.5), 10)
 @test gradtest(x -> dropout(copy(RNG), x, 0.4; dims=1), (8, 9))
 
